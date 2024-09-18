c93f0d5361e0bbddae5b8f5c3acec525407e3ab9
Puneet Pandey <ppuneet64@gmail.com>
2024-09-18 16:46:11 +0530

    Added For New Course

--- README.md
+++
@@ -1,3 +0,0 @@
-# udemy-TESTING-LIBRARY
-
-Code to accompany [React Testing Library and Jest](https://www.udemy.com/course/react-testing-library/?couponCode=TEST-LIB-GITHUB) course on Udemy.
--- vite-starter/src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
-});
